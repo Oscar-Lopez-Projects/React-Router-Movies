d49828e7503877d6d80b17f9a6f842199081f5af
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-04-27 11:28:51 -0700

    finished mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, {useState} from 'react';
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
   return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <SavedList list={savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={Movie} />
     </div>
+    
   );
 };
 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
-const MovieList = props => {
+const MovieList = () => {
+
+  const [movies, setMovies]= useState([]);
+
+  useEffect(()=>{
+    const getMovies= ()=>{
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovies(response.data);
+        })
+        .catch(error =>{
+          console.error('server', error);
+        })
+    }
+
+    getMovies();
+  }, []);
+  
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie => (
+        <Link to={`movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
